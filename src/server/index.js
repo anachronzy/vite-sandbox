c40c737edb05316aca33827b225d3a7d77a90e3c
aiproshkin <al.pro@list.ru>
2024-04-14 23:32:00 +0500

    init

---
+++ src/server/index.js
@@ -0,0 +1,33 @@
+import { readFileSync } from 'node:fs';
+import express from 'express';
+import { createServer as createViteServer } from 'vite';
+
+const PORT = +process.env.PORT || 3000;
+
+const createServer = async () => {
+  const app = express();
+
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+  });
+
+  app.use(vite.middlewares);
+
+  app.use('*', async (req, res, next) => {
+    try {
+      let html = readFileSync('index.html', 'utf-8');
+      html = await vite.transformIndexHtml(req.url, html);
+
+      res.set({ 'Content-Type': 'text/html' }).send(html);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+  });
+};
+
+createServer().catch((err) => console.error(err));
