c442761b63fc5f84fa868a728e9dd398fdf4f5fc
aiproshkin <al.pro@list.ru>
2024-04-15 00:06:28 +0500

    feat: start everything

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'node:fs';
 import express from 'express';
 import { createServer as createViteServer } from 'vite';
 
-const PORT = +process.env.PORT || 3000;
+const PORT = +(process.env.PORT || 0) || 3000;
 
 const createServer = async () => {
   const app = express();
@@ -14,15 +14,11 @@ const createServer = async () => {
 
   app.use(vite.middlewares);
 
-  app.use('*', async (req, res, next) => {
-    try {
-      let html = readFileSync('index.html', 'utf-8');
-      html = await vite.transformIndexHtml(req.url, html);
+  app.use('*', async (req, res) => {
+    let html = readFileSync('index.html', 'utf-8');
+    html = await vite.transformIndexHtml(req.url, html);
 
-      res.set({ 'Content-Type': 'text/html' }).send(html);
-    } catch (error) {
-      return next(error);
-    }
+    res.set({ 'Content-Type': 'text/html' }).send(html);
   });
 
   app.listen(PORT, () => {
