9578eb989f6c90d8cedf6d6e6038882898148a64
aiproshkin <al.pro@list.ru>
2024-04-21 18:52:08 +0500

    simple ssr

--- src/server/index.ts
+++ src/server/index.ts
@@ -14,11 +14,24 @@ const createServer = async () => {
 
   app.use(vite.middlewares);
 
-  app.use('*', async (req, res) => {
-    let html = readFileSync('index.html', 'utf-8');
-    html = await vite.transformIndexHtml(req.url, html);
+  app.use('*', async (req, res, next) => {
+    const url = req.originalUrl;
 
-    res.set({ 'Content-Type': 'text/html' }).send(html);
+    try {
+      let template = readFileSync('index.html', 'utf-8');
+      template = await vite.transformIndexHtml(url, template);
+      const { render } = await vite.ssrLoadModule(
+        'src/client/entry-server.tsx',
+      );
+      const appHtml = render(url);
+      const html = template.replace('<!--ssr-outlet-->', appHtml);
+      res.set({ 'Content-Type': 'text/html' }).end(html);
+    } catch (e) {
+      if (e instanceof Error) {
+        vite.ssrFixStacktrace(e);
+      }
+      next(e);
+    }
   });
 
   app.listen(PORT, () => {
